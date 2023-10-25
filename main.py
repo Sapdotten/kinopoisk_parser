1478dea721f70912e4158bf101e73dcd167dcd3b
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-10-26 01:25:06 +0400

    parser is save the page and the count of good rewievs

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 import re
 import json
 import yaml
+from typing import Union
 
 def get_configs():
     """retrieves the settings and breakpoint from the settings file"""
@@ -53,12 +54,18 @@ bad_reviews = Template("https://www.kinopoisk.ru/film/${film_id}/reviews/ord/dat
 url = "https://www.kinopoisk.ru/top/navigator/m_act[rating]/1%3A/order/num_vote/perpage/200/#results"
 chrome = UserAgent().chrome
 
-def get_page(link: str) -> str:
-    """return a html of page with rewievs"""
+def get_page(link: str) -> BeautifulSoup:
+    """tries to get soup-page without captcha"""
     global chrome
-    slep()
-    html_text = requests.get(link, headers={'User-Agent': chrome}).text
-    return html_text
+    while True:
+        html = requests.get(link, headers={'User-Agent': chrome}).text
+        slep()
+        page = BeautifulSoup(html, 'lxml')
+        captcha = page.find_all('div', class_ = "CheckboxCaptcha")
+        if (captcha is None) or len(captcha)==0:
+            print("page is gotten")
+            return page
+        print("fail, is catpcha")
 
 def save_rewievs_from_page(page: BeautifulSoup, film_name: str):
     """Save rewievs for film from on of his page"""
@@ -69,50 +76,54 @@ def save_rewievs_from_page(page: BeautifulSoup, film_name: str):
             break
         configs["count_of_good_rewievs"]+=1
         sync_configs()
-        num = format(i+configs["count_of_good_rewievs"], '04d')
+        num = format(configs["count_of_good_rewievs"], '04d')
         with open(f'dataset\\good\\{num}.txt', 'w+', encoding='utf8') as file:
             file.write(film_name+'\n'+rewiev.text)
 
+
 def save_good_rewievs(film_id: str, film_name: str, stop_page: int = 0):
-    html = ''
-    page = None
-    count_of_rewievs = 0
-    while True:
-        link = good_reviews.substitute(film_id = film_id, page_num =1)
-        html = get_page(link)
-        print(f"try to get page of {film_name}")
-        page = BeautifulSoup(html, 'lxml')
-        count_of_rewievs = page.find('div', class_ = 'pagesFromTo')
-        if count_of_rewievs is not None:
-            sync_configs()
-            break
+    """saves good rewievs from one film"""
+    global configs
+    link = good_reviews.substitute(film_id = film_id, page_num=configs["page_num"])
+    page = get_page(link)
+    
+    #парсим количество хороших отзывывов
+    count_of_rewievs = page.find('div', class_ = 'pagesFromTo')
     count_of_rewievs = count_of_rewievs.text
     count_of_rewievs = int(re.sub('\d+.\d+ из ', '', count_of_rewievs, count=0))
     print(f"In film {film_name} {count_of_rewievs} good rewievs")
 
-    #подсчет количества страниц одного фильма
+    #подсчет количества страниц с хорошими отзывами одного фильма ------#
     count_of_pages = 0
     if count_of_rewievs>=200:
-        count_of_pages = count_of_rewievs//200 #получаем количетсво страниц с отзывами
-        if count_of_pages > count_of_rewievs*200:
+        count_of_pages = count_of_rewievs//200
+        if count_of_rewievs > count_of_pages*200:
             count_of_pages+=1
     else:
         count_of_pages = 1
-    ####
+    print(f'Count of pages with good rewievs for film "{film_name}" is {count_of_pages}')
+    #------------------------------------------------------------------#
 
-    print(f"Count of pages with good rewievs for film {film_name} is {count_of_pages}")
-    rewievs = page.find_all('div', class_ = 'brand_words')
-    for i, rewiev in enumerate(rewievs):
+    save_rewievs_from_page(page, film_name)
+    configs["page_num"]+=1
+    sync_configs()
+    page_num = configs["page_num"]
+    for i in range(page_num, count_of_pages+1, 1):
+        print(f"saving a {i} page")
+        link = good_reviews.substitute(film_id = film_id, page_num=i)
+        page = get_page(link)
+        save_rewievs_from_page(page, film_name)
+        configs["page_num"]+=1
+        sync_configs()
+    configs["page_num"]=1
+    sync_configs()
 
         
-
-
-
-
 if __name__=="__main__":
     get_configs()
     create_folders()
     films = {}
 with open('films.json', 'r') as file:
     films = json.load(file)
-    save_good_rews("1143242", films["1143242"]['name'])
+save_good_rewievs("535341", films["535341"]['name'])
+
