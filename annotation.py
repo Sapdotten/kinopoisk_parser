fa9850a87bbb5c50a96c71a04e16da48ba45aac5
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-10-31 01:25:49 +0400

    separate make_annotation to 2 diffrnt funcs

--- annotation.py
+++ annotation.py
@@ -3,6 +3,18 @@ from os import listdir, path
 from os.path import  join, abspath
 
 
+def add_line(annotation_file: path, path_: path, class_name: str) -> None:
+    """Add a line about file to .csv file
+
+    Args:
+        annotation_file (path): path to annotation file
+        path_ (path): path to file of dataset
+        class_name (str): name of class of file
+    """
+    with open(annotation_file, 'a+') as file:
+        fw = csv.writer(file, delimiter = ",", lineterminator="\r")
+        fw.writerow([abspath(path_), path_, class_name])
+
 def make_annotation(annotation_file: path) -> None:
     """Build an annotation file
 
@@ -12,12 +24,9 @@ def make_annotation(annotation_file: path) -> None:
     classes = ["good", "bad"]
     for class_ in classes:
         dir = join('dataset', class_)
-        with open(annotation_file, 'a+') as file:
-            fw = csv.writer(file, delimiter = ",", lineterminator="\r")
-            for elem in listdir(dir):
-                fw.writerow([abspath(join(dir, elem)),
-                            join(dir, elem),
-                            class_])
+        for elem in listdir(dir):
+            add_line(annotation_file, join(dir, elem), class_)
+
     
         
 
