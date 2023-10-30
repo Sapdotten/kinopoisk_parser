d0f674f11b141454515ee488352198e018550802
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-10-31 00:18:14 +0400

    Divide "make_annotation" to pair of funcs, on of them is adding lines to csv file

--- annotation.py
+++ annotation.py
@@ -1,25 +1,36 @@
 import csv
-from os import listdir
-from os.path import isfile, join, abspath
+from os import listdir, path
+from os.path import  join, abspath
 
+def fill_csv(annotaton_file: path ,dir: path, class_name: str) -> None:
+    """addes a notes about files in dir in a annotation file
 
-def make_annotation():
-    good_dir = join("dataset", "good")
-    bad_dir = join("dataset", "bad")
-    with open("annotation.csv", "w") as f:
-        fw = csv.writer(f, delimiter = ",", lineterminator="\r")
-        for elem in listdir(good_dir):
-            fw.writerow([abspath(join(good_dir, elem)),
-                        join(good_dir, elem),
-                        "good"])
-        for elem in listdir(good_dir):
-            fw.writerow([abspath(join(bad_dir, elem)),
-                        join(good_dir, elem),
-                        "bad"])
+    Args:
+        annotaton_file (path): dir where will be saved annotation
+        dir (path): dir with dataset
+        class_name (str): class of dataset
+    """
+    with open(annotaton_file, 'a+') as file:
+        fw = csv.writer(file, delimiter = ",", lineterminator="\r")
+        for elem in listdir(dir):
+            fw.writerow([abspath(join(dir, elem)),
+                         join(dir, elem),
+                         class_name])
+
+def make_annotation(annotation_file: path) -> None:
+    """Build an annotation file
+
+    Args:
+        annotation_file (path): dir where will be saved annotation
+    """
+    dataset_dir = 'dataset'
+    good_dir = join(dataset_dir, "good")
+    bad_dir = join(dataset_dir, "bad")
+    fill_csv(annotation_file, good_dir, "good")
+    fill_csv(annotation_file, bad_dir, "bad")
+    
         
 
-
-
 if __name__ == "__main__":
-    make_annotation()
+    make_annotation("annotation.csv")
     print("Annotation is done")
