36dde2f1d961b408a246e046b88843439e241485
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-10-31 01:13:03 +0400

    Join a classnames to list, merge two func to one

--- annotation.py
+++ annotation.py
@@ -2,20 +2,6 @@ import csv
 from os import listdir, path
 from os.path import  join, abspath
 
-def fill_csv(annotaton_file: path ,dir: path, class_name: str) -> None:
-    """addes a notes about files in dir in a annotation file
-
-    Args:
-        annotaton_file (path): dir where will be saved annotation
-        dir (path): dir with dataset
-        class_name (str): class of dataset
-    """
-    with open(annotaton_file, 'a+') as file:
-        fw = csv.writer(file, delimiter = ",", lineterminator="\r")
-        for elem in listdir(dir):
-            fw.writerow([abspath(join(dir, elem)),
-                         join(dir, elem),
-                         class_name])
 
 def make_annotation(annotation_file: path) -> None:
     """Build an annotation file
@@ -23,11 +9,15 @@ def make_annotation(annotation_file: path) -> None:
     Args:
         annotation_file (path): dir where will be saved annotation
     """
-    dataset_dir = 'dataset'
-    good_dir = join(dataset_dir, "good")
-    bad_dir = join(dataset_dir, "bad")
-    fill_csv(annotation_file, good_dir, "good")
-    fill_csv(annotation_file, bad_dir, "bad")
+    classes = ["good", "bad"]
+    for class_ in classes:
+        dir = join('dataset', class_)
+        with open(annotation_file, 'a+') as file:
+            fw = csv.writer(file, delimiter = ",", lineterminator="\r")
+            for elem in listdir(dir):
+                fw.writerow([abspath(join(dir, elem)),
+                            join(dir, elem),
+                            class_])
     
         
 
