61efc7ad3d3583185a814877b070f4b7962f26c6
Sapdotten <semenova.a.p.04@gmail.com>
2023-12-12 03:56:42 +0400

    изменена логика итератора

--- task_5.py
+++ task_5.py
@@ -1,34 +1,32 @@
 import csv
-from typing import Union
+import os
 
 
 class FilesIterator:
-    returned_files: set
-    file: str
-    def __init__(self, annotation_file: str, class_: str):
+    def __init__(self, dir: str, class_: str):
         """
         Args:
             annotation_file (str): file with annotation for dataset
             class_ (str): class for iteration
         """
         self.class_ = class_
-        self.file = annotation_file
+        self.dir = dir
         self.returned_files = set()
 
     def __iter__(self):
         return self
-    
+
     def __next__(self):
-        with open(self.file, 'r+') as file:
-            fr = csv.reader(file, delimiter = ',', lineterminator="\r")
-            for row in fr:
-                if row[2]==self.class_ and row[0] not in self.returned_files:
-                    self.returned_files.add(row[0])
-                    return row[0]
-            raise StopIteration
+
+        for filename in os.listdir(os.path.join(self.dir, self.class_)):
+            f = os.path.join(os.path.join(self.dir, self.class_, filename))
+            if f not in self.returned_files:
+                self.returned_files.add(f)
+                return f
+        raise StopIteration
 
 
-if __name__=='__main__':
-    tr = FilesIterator('task_3_annotation.csv', 'bad')
+if __name__ == '__main__':
+    tr = FilesIterator('dataset', 'bad')
     for elem in tr:
         print(elem)
